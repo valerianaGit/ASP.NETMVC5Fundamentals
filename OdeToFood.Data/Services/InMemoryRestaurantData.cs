f02de95d44c79ef907a964a70e37de861393833f
VRuiz <valeria.do@gmail.com>
2021-05-11 12:31:58 -0500

    fix using statements to be able to use Restaurant

--- OdeToFood.Data/Services/InMemoryRestaurantData.cs
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using OdeToFood.Data.Models;
+
 namespace OdeToFood.Data.Services
 {
-    public class InMemoryRestaurantData
-    {
-        public InMemoryRestaurantData()
+  
+        public class InMemoryRestaurantData : IRestaurantData
         {
+            public IEnumerable<Restaurant> GetAll()
+            {
+                throw new NotImplementedException();
+            }
+
+        IEnumerable<Restaurant> IRestaurantData.GetAll()
+        {
+            throw new NotImplementedException();
         }
     }
+    
 }
