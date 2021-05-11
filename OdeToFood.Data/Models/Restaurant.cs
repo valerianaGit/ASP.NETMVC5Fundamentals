aca5bfae3f5875f74ca642bcc031caa90c3c8d47
VRuiz <valeria.do@gmail.com>
2021-05-11 12:21:16 -0500

    create Restaurant properties

--- OdeToFood.Data/Models/Restaurant.cs
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -6,5 +6,13 @@ namespace OdeToFood.Data.Models
         public Restaurant()
         {
         }
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public string Cuisine { get; set; }
+
+
+
+
     }
 }
